d1ceef7a38e1c0610c7d1fd6fbc67c04feeb96e3
Dominik Fuchß <develop@fuchss.org>
2022-02-04 21:27:25 +0100

    Added initial version of server code

---
+++ src/main/kotlin/com/github/jambeez/server/JamBeezServerApplication.kt
@@ -0,0 +1,11 @@
+package com.github.jambeez.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JamBeezServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<JamBeezServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/jambeez/server/JamBeezServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.jambeez.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JamBeezServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
