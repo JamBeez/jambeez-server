e0b7f21f2d2d6615c8496ebf224cdfa43e14578d
Dominik Fuchß <develop@fuchss.org>
2022-02-06 10:36:39 +0100

    Added default page

---
+++ src/main/kotlin/com/github/jambeez/server/config/WebConfig.kt
@@ -0,0 +1,13 @@
+package com.github.jambeez.server.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+}
